c208e96ddb716cf7066687ea0f20938d992302f1
Eduard Rudnev <eduardrudnev97@gmail.com>
2021-10-01 21:53:18 +0300

    main: formated checkbox

--- src/components/RockCheckbox.vue
+++ src/components/RockCheckbox.vue
@@ -1,15 +1,9 @@
 <template>
   <form @submit.prevent="handleSubmit" class="rock-checkbox">
     <div class="rock-checkbox__wrapper-input">
-      <div
-				class="rock-checkbox__fake-input"
-			>
-				<img
-					v-if="field"
-					src="../assets/icons/tick.svg"
-					alt="tick"
-				>
-			</div>
+      <div class="rock-checkbox__fake-input">
+        <img v-if="field" src="../assets/icons/tick.svg" alt="tick" />
+      </div>
 
       <input type="checkbox" v-model="field" class="rock-checkbox__input" />
       <label class="rock-checkbox__label" for="accept">
@@ -23,7 +17,7 @@
 export default {
   data() {
     return {
-      field: false,
+      field: false
     };
   },
   methods: {
@@ -39,40 +33,40 @@ export default {
   outline: 1px solid red;
   &__wrapper-input {
     position: relative;
-		min-height: 20px;
+    min-height: 20px;
   }
 
   &__fake-input {
-		top: calc(50% - 10px);
-		position: absolute;
+    top: calc(50% - 10px);
+    position: absolute;
     width: 20px;
     height: 20px;
     background: $success;
     border-radius: 2px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-		& img {
-			width: 10px;
-			height: 8px;
-		}
+    & img {
+      width: 10px;
+      height: 8px;
+    }
   }
 
   &__input {
     opacity: 0;
-		position: absolute;
-		width: 20px;
+    position: absolute;
+    width: 20px;
     height: 20px;
-		cursor: pointer;
+    cursor: pointer;
   }
 
-	&__label {
-		display: block;
-		width: 100%;
-		height: 20px;
-		padding-left: 32px;
-		line-height: 20px;
-	}
+  &__label {
+    display: block;
+    width: 100%;
+    height: 20px;
+    padding-left: 32px;
+    line-height: 20px;
+  }
 }
 </style>
