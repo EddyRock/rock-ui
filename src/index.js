da1c28f1f02edf88b2c1038dbb25a6df09f9b5a1
Eduard Rudnev <eduardrudnev97@gmail.com>
2021-10-02 23:22:11 +0300

    main: first publish

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import RockInput from './components/RockInput.vue';
+import RockCheckbox from './components/RockCheckbox.vue';
+import RockSelect from './components/RockSelect.vue';
+import RockTextArea from './components/RockTextArea.vue';
 
 const Componetns = {
   RockInput,
+  RockCheckbox,
+  RockSelect,
+  RockTextArea,
 };
 
 Object.keys(Componetns).forEach((name) => {
