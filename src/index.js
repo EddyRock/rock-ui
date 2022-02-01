cc9646306bb438a8c9b9ed14262addeee978a4fd
Eduard Rudnev <eduard.rudnev@x5.ru>
2022-02-01 23:21:32 +0300

    feat: working version of lib

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
-import Vue from 'vue';
-import RockInput from './components/RockInput.vue';
-import RockCheckbox from './components/RockCheckbox.vue';
-import RockSelect from './components/RockSelect.vue';
-import RockTextArea from './components/RockTextArea.vue';
-import RockPaginator from './components/RockPaginator.vue';
-import RockRadiobutton from './components/RockRadiobutton.vue';
-import RockLoader from './components/RockLoader.vue';
-import RockLabel from './components/RockLabel.vue';
-import RockProgressbar from './components/RockProgressbar.vue';
-import RockButton from './components/RockButton.vue';
-import RockIconButton from './components/RockIconButton.vue';
-
-const Componetns = {
-  RockInput,
-  RockCheckbox,
-  RockSelect,
-  RockTextArea,
-  RockPaginator,
-  RockRadiobutton,
-  RockLoader,
-  RockLabel,
-  RockProgressbar,
-  RockButton,
-  RockIconButton,
-};
-
-Object.keys(Componetns).forEach((name) => {
-  Vue.component(name, Componetns[name]);
-});
-
-export default Componetns;
+export { default as RockInput } from './components/RockInput.vue';
+export { default as RockCheckbox } from './components/RockCheckbox.vue';
+export { default as RockSelect } from './components/RockSelect.vue';
+export { default as RockTextArea } from './components/RockTextArea.vue';
+export { default as RockPaginator } from './components/RockPaginator.vue';
+export { default as RockRadiobutton } from './components/RockRadiobutton.vue';
+export { default as RockLoader } from './components/RockLoader.vue';
+export { default as RockLabel } from './components/RockLabel.vue';
+export { default as RockProgressbar } from './components/RockProgressbar.vue';
+export { default as RockButton } from './components/RockButton.vue';
+export { default as RockIconButton } from './components/RockIconButton.vue';
